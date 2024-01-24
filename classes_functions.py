f7a8b6ceccdd0c526ddb87f6137af08e97a0915e
HL-2002 <henryslang8231682@gmail.com>
2024-01-24 13:22:59 -0400

    Update with separation of modules, as well as refactoring with documentation

---
+++ classes_functions.py
@@ -0,0 +1,134 @@
+class File:
+    """Class that represents a file in the system, including its metadata and content
+    for listing and indexing purposes."""
+
+    def __init__(self, name, size, creationDate, modifyDate, extension, content):
+        self.name = name
+        self.size = size  # bytes
+        self.creationDate = creationDate  # timestamp
+        self.modifyDate = modifyDate  # timestamp
+        self.extension = extension  # txt, pdf, etc.
+        self.content = content  # text, binary, etc.
+
+
+class Folder:
+    """Class that represents a folder in the system, including its metadata and content
+    for listing and indexing purposes."""
+
+    def __init__(self, name, creationDate, modifyDate):
+        self.name = name
+        self.creationDate = creationDate  # timestamp
+        self.modifyDate = modifyDate  # timestamp
+        self.files = []
+        self.folders = []
+        self.size = 0  # bytes
+
+
+class Unit:
+    """Class that represents a storage unit in the system, including its
+    metadata and content for indexing purposes."""
+
+    # Dictionary that contains all the units in the system
+    units = dict()
+
+    def __init__(self, name, totalSize, freeSize, type):
+        self.name = name
+        self.totalSize = totalSize  # bytes
+        self.freeSize = freeSize  # bytes
+        self.type = type  # HDD,SSD,USB,etc.
+        self.folders = []
+
+        # Add unit to the units dictionary
+        if self.name not in Unit.units:
+            Unit.units[self.name] = self
+
+
+class Command:
+    """Class that represents a command in the system, including methods to 
+    explain and execute it."""
+
+    # Dictionary that contains all the commands in the system
+    commands = dict()
+
+    def __init__(self, name, role, description, manual, call):
+        self.name = name
+        self.description = description  # Command's short description
+        self.role = role  # Command's role restriction
+        self.call = call  # Command's function
+        self.manual = manual  # Command's usage explanation
+
+        # Add command to the commands dictionary
+        if self.name not in Command.commands:
+            Command.commands[self.name] = self
+
+    def __str__(self):
+        return self.description
+
+    def __call__(self, *arg):
+        self.call(*arg)
+
+
+class User:
+    """Class that represents a user in the system for login and command access
+    purposes."""
+
+    # Dictionary that contains all the users in the system
+    users = dict()
+
+    def __init__(self, name, password, role):
+        self.name = name
+        self.password = password
+        self.role = role
+
+        if self.name not in User.users:
+            User.users[self.name] = self
+
+def help():
+    # Command process
+    for name in Command.commands:
+        print(Command.commands[name])
+    
+
+
+def man(arg:list):
+    # Argument validation
+    if len(arg) == 1:
+        if arg[0] in Command.commands:
+            # Command process
+            print(Command.commands[arg[0]].manual)
+        else:
+            print("Ninguna entrada del manual para " + arg[0])
+    else:
+        print("argumentos incorrectos")
+
+def ls(arg:list):
+    unidad = arg[0]
+    ubication = arg[1]
+    if unidad in Unit.units:
+        print("unidad encontrada")
+        if ubication == "/":
+            print("raiz")
+        else:
+            print("no raiz")
+    else:
+        print("unidad no encontrada")
+
+
+def login() -> User:
+    # Command process
+    name = input("user: ")
+    password = input("password: ")
+    if name in User.users:
+        if User.users[name].password == password:
+            print("Bienvenido " + name)
+            return User.users[name]
+        else:
+            print("password incorrecto")
+    else:
+        print("usuario no encontrado")
+
+    return None
+    
+
+def dir(*arg):
+    ...
---
+++ shell.py
@@ -0,0 +1,141 @@
+from classes_functions import *
+
+class Shell:
+    """Class that represents the shell of the system.
+    It contains the main loop of the program, as well as the commands and
+    users in the system.
+    It's important to note that:
+    * the shell is the only class that has access to the commands, users and units.
+    * the shell not only executes the prompts, but validates the user's role."""
+
+    def __init__(self):
+        self.currentUser: User = None
+
+        # List of commands in the system
+        Command(
+            "help",
+            "any",
+            "help - muestra los comandos disponibles y una breve descripción de los mismos",
+            """help
+
+                Proporciona una lista de los comandos disponibles en el sistema.""",
+            help,
+        )
+
+        Command(
+            "man",
+            "any",
+            "man - muestra la descripción del comando especificado",
+            """man [comando] 
+                Describe el comando especificado y su uso.""",
+            man,
+        )
+
+        Command(
+            "login",
+            "any",
+            "login - inicia sesión en el sistema",
+            """login 
+                Inicia prompt para ingresar usuario y contraseña.""",
+            login,
+        )
+
+        Command(
+            "exit",
+            "any",
+            "exit - cierra la sesión actual",
+            """exit
+                
+                Cierra la sesión actual y regresa al prompt de login.""",
+            exit)
+
+        Command(
+            "dir",
+            "any",
+            "dir - lista los archivos y carpetas en la ubicación dada",
+            """dir [unidad] [ubicación]
+
+                Lista los archivos y carpetas en la unidad y ubicación dadas.
+                Al no usar argumentos, lista las unidades disponibles.
+                Al especificar [unidad], pero no [ubicación], lista las carpetas y archivos en la raíz de la unidad.
+
+                También es posible especificar los siguientes argumentos:
+
+                * [criterio]: -LastUpdate, -creation, -range
+                  Crea la lista según el criterio especificado.
+
+                  -LastUpdate: lista los archivos y carpetas según la fecha de última modificación.
+                  -creation: lista los archivos y carpetas según la fecha de creación.
+                  -range: lista los archivos y carpetas según un rango de tamaño.
+                          posterior a él, se especifica el rango como:
+                          * min-max: intervalo de tamaño en bytes de la forma min-max.
+                          * tamaño=|>|<: tamaño exacto, mayor o menor al especificado.
+
+                          al listar con range, es posible especificar el argumento -ext para filtrar por extensión.
+
+                * [orden]: asc o desc
+                  Ordena los resultados de la lista de forma ascendente o descendente.""",
+            dir,
+        )
+
+        # User for testing purposes
+        User("admin", "admin", "admin")
+
+    def loop(self):
+        prompt = ""
+        print(
+            "Bienvenido a la MFShell",
+            "para ver los comandos disponibles escriba help",
+            sep="\n",
+        )
+
+        while True:
+            # User setup
+            name = ""
+            if self.currentUser != None:
+                name = self.currentUser.name
+            else:
+                name = "guest"
+
+            # Prompt loop
+            prompt = ""
+            try:
+                prompt = input(name + "@" + name + ":~ /$ ")
+            except KeyboardInterrupt:
+                exit(0)
+            prompt = prompt.split(" ")
+
+            if (
+                self.currentUser == None
+                and prompt[0] not in ["login", "help", "exit"]
+            ):
+                print("no hay usuario logueado, por favor utilizar comando login")
+                continue
+
+            # Command role validation and execution
+            if prompt[0] in Command.commands:
+                command = Command.commands[prompt[0]]
+
+                # Validate role
+                if command.role != "any":
+                    if self.currentUser == None:
+                        print("must login to access command")
+                        continue
+                    elif self.currentUser.role != command.role:
+                        print("user doesn't have access to command")
+                        continue
+                
+                # Command execution if it hasn't been invalidated
+                if len(prompt) > 1:
+                    try:
+                        command(prompt[1:])
+                    except TypeError:
+                        print("function doesn't take arguments")
+                else:
+                    # Check for login command to update current user
+                    if command != Command.commands["login"]:
+                        command()
+                    else:
+                        self.currentUser = login()
+            else:
+                print("command not found")
